38ea60ebb6b8c6c4d43dd119a450ec929119e1e2
mauwqq <mauw0@protonmail.com>
2024-08-14 11:53:04 -0300

    TP1/E2 actualizado

---
+++ TPs/TP1/E2.py
@@ -0,0 +1,29 @@
+'''
+Desarrollar una función que reciba tres números enteros positivos correspondientes
+al día, mes, año de una fecha y verifique si corresponden a una fecha válida. Debe
+tenerse en cuenta la cantidad de días de cada mes, incluyendo los años bisiestos.
+Devolver True o False según la fecha sea correcta o no. Realizar también un
+programa para verificar el comportamiento de la función.
+'''
+
+meses = {1: 31,
+         2: 28,
+         3: 31,
+         4: 30,
+         5: 31,
+         6: 30,
+         7: 31,
+         8: 31,
+         9: 30,
+         10: 31,
+         11: 30,
+         12: 31
+         }
+
+
+def main():
+    pass 
+
+
+if __name__ == '__main__':
+    main()
