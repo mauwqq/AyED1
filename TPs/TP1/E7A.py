a50630d20d8050fbc1f88b36b0704d43b4266477
mauwqq <mauw0@protonmail.com>
2024-09-17 17:36:39 -0300

    Mejorada la lógica haciendo que los bucles salgan lo antes posible, documentación a todas las funciones añadidas.

--- TPs/TP1/E7A.py
+++ TPs/TP1/E7A.py
@@ -23,6 +23,14 @@ meses = {
 
 
 def pedir_fecha() -> list[int]:
+    """Solicita la fecha al usuario verificado cada valor ingresado. Primero
+    solicita el año, si es bisiesto actualiza la cantidad de dias de febrero en
+    meses. Despues solicita el mes que comprueba con comprobar_mes y por ultimo
+    solicita el dia.
+
+    Post: Devuelve una lista de tres enteros con la fecha en formato YY/MM/DD.
+
+    """
     fecha = []
     texto = ["Año", "Mes", "Dia"]
     print("Ingrese la fecha")
@@ -40,47 +48,89 @@ def pedir_fecha() -> list[int]:
                             if comprobar_mes(num):
                                 fecha.append(num)
                             else:
-                                print("Mes invalido, reintentar.")
+                                print("Mes inválido, reintentar.")
                                 continue
                         case 2:  # dia
                             if comprobar_dia(num, fecha[1]):
                                 fecha.append(num)
                             else:
-                                print("Dia invalido, reintentar.")
+                                print("Día inválido, reintentar.")
                                 continue
                     break
-                print("El numero ingresado debe ser positivo.")
+                print("El número ingresado debe ser positivo.")
             except ValueError:
-                print("Debe ingresar un numero, reintentar.")
+                print("Debe ingresar un número, reintentar.")
     return fecha
 
 
 def es_bisiesto(anio: int) -> bool:
+    """Comprueba si un año es bisiesto.
+
+    Pre: Recibe el año como un número entero.
+
+    Post: Si el entero es divisible por cuatro y no es divisible
+          por 100, o es divisible por 400 es bisiesto y devuelve True, si no False.
+
+    """
     return (anio % 4 == 0) and (anio % 100 != 0) or (anio % 400 == 0)
 
 
 def comprobar_mes(mes: int) -> bool:
-    return mes <= len(meses.keys())
+    """Comprueba si el mes dado como parámetro es válido.
+
+    Pre: Recibe el mes como un número entero positivo.
+
+    Post: Si el número está en las claves del diccionario, meses devuelve True.
+          Si el número no está en las claves del diccionario, meses devuelve
+          False.
+
+    """
+    return mes in meses.keys()
 
 
 def comprobar_dia(dia: int, mes: int) -> bool:
+    """Comprueba que el día ingresado por el usuario sea válido verificando que
+    sea menor o igual al valor del mes en meses.
+
+    Pre: día es un número entero positivo.
+         mes es un número entero positivo.
+
+    Post: Devuelve True si día es menor o igual a la cantidad de días del mes.
+          Devuelve False si día es mayor a la cantidad de días del mes.
+
+    """
     return dia <= meses.get(mes)
 
 
 def pedir_cuanto_adelantar() -> int:
+    """Solicita al usuario un número entero positivo y lo devuelve.
+
+    Post: Devuelve un número entero positivo.
+
+    """
     while True:
         try:
-            num = int(input("cuantos dias quiere sumar a la fecha: "))
+            num = int(input("Cuantos días quiere sumar a la fecha: "))
             if num > 0:
                 break
-            print("El numero ingresado debe ser positivo.")
+            print("El número ingresado debe ser positivo.")
         except ValueError:
-            print("Debe ingresar un numero, reintentar.")
+            print("Debe ingresar un número, reintentar.")
     return num
 
 
 def diasiguiente(dia: int, mes: int, anio: int) -> tuple[int]:
-    if (dia + 1) > meses.get(mes):
+    """Suma un dia a la fecha ingresada.
+
+    Pre: dia, mes y anio son números enteros positivo.
+
+    Post: Devuelve una tupla de tres enteros(dia, mes, año) con la fecha
+          modificada.
+
+    """
+    if (dia + 1) < meses.get(mes):
+        dia += 1
+    else:
         if mes != 12:
             mes += 1
             dia = 1
@@ -88,12 +138,18 @@ def diasiguiente(dia: int, mes: int, anio: int) -> tuple[int]:
             anio += 1
             mes = 1
             dia = 1
-    else:
-        dia += 1
     return dia, mes, anio
 
 
 def calcular_fecha(adelanto: int, dia: int, mes: int, anio: int) -> tuple[int]:
+    """Bucle que ejecuta diasiguiente las veces que sea necesario para llegar
+    a la fecha deseada.
+
+    Pre: adelanto, dia, mes, anio son numeros enteros positivos.
+
+    Post: Devuelve la fecha en una tupla(dia,mes,año).
+
+    """
     i = 0
     while i < adelanto:
         dia, mes, anio = diasiguiente(dia, mes, anio)
@@ -106,7 +162,7 @@ def main() -> None:
     print(f"La fecha es: {dia}/{mes}/{anio}")
     adelanto = pedir_cuanto_adelantar()
     dia, mes, anio = calcular_fecha(adelanto, dia, mes, anio)
-    print(f"{adelanto} dias despues: {dia}/{mes}/{anio}")
+    print(f"{adelanto} días después: {dia}/{mes}/{anio}")
 
 
 if __name__ == "__main__":
