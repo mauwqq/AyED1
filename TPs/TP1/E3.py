ac594a6dacdd905d7475f4b05071b4f428332533
mauwqq <mauw0@protonmail.com>
2024-09-17 15:12:46 -0300

    Contratos de funciones corregidos y añadidos.

--- TPs/TP1/E3.py
+++ TPs/TP1/E3.py
@@ -9,29 +9,34 @@ Realizar también un programa para verificar el comportamiento de la función.
 
 
 def pedir_numero(msj: str) -> int:
-    """Recibe un mensaje, lo muestra, guarda y retorna un numero entero
-    positivo.
-    Pre -> Recibe un string, el mensaje que va a mostrar el input para
-        recibir el numero.
-    Post -> Retorna el valor ingresado si es un numero entero positivo.
+    """Solicita al usuario un número entero positivo y lo devuelve.
+
+    Pre: Recibe un string, el mensaje que va a mostrar el input para
+         recibir el número.
+
+    Post: Retorna el valor ingresado si es un número entero positivo.
+
     """
     while True:
         try:
             n = int(input(msj))
             if n > 0:
                 break
-            print("El numero tiene que ser positivo.")
+            print("El número tiene que ser positivo.")
         except ValueError:
-            print("Debe ingresar un numero.")
+            print("Debe ingresar un número.")
     return n
 
 
 def calcular_pasaje(viajes: int, importe: int) -> tuple[float]:
-    """Calcula el descuento de los pasajes y los devuelve
-    Pre -> Recibe la cantidad de viajes como entero positivo,
-        recibe el precio de esos viajes como entero positivo.
-    Post -> Devuelve el importe con descuento y el descuento en,
-        una tupla de flotantes.
+    """Calcula el descuento de los pasajes y los devuelve.
+
+    Pre: Recibe la cantidad de viajes como entero positivo.
+         Recibe el precio de esos viajes como entero positivo.
+    
+    Post: Devuelve el importe con descuento y el descuento en una tupla de
+          flotantes.
+    
     """
     importe_final = 0
     desc = 0.0
@@ -49,6 +54,19 @@ def calcular_pasaje(viajes: int, importe: int) -> tuple[float]:
 
 
 def imprimir_resultado(viajes: int, importe_final: int, desc: float) -> None:
+    """Imprime un resumen del descuento aplicado y el importe total calculado.
+    Muestra al usuario el total de viajes, el porcentaje de descuento aplicado
+    y el importe final a pagar. Si el importe final es -1, se indica que se debe
+    consultar el valor actualizado en Internet.
+
+    Pre: Recibe viajes como entero positivo.
+         Recibe importe_final como entero positivo o -1 si no esta disponible.
+         Recibe desc en número flotante.
+
+    Post: Si importe_final no es -1 imprime el resumen.
+          Si importe_final es -1 informa que averigue el precio en internet.
+
+    """
     if importe_final != -1:
         print(
             f"El total de viajes fue: {viajes}",
