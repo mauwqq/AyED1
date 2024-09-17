869bbda9d109dfa91b58571b130fe6034d356b0b
mauwqq <mauw0@protonmail.com>
2024-09-17 14:34:53 -0300

    Corregí errores gramaticales, utilice reduce, map y funciones lambda para simplificar el código y mejore los contratos de las funciones.

--- TPs/TP1/E1.py
+++ TPs/TP1/E1.py
@@ -7,52 +7,55 @@ valores, invocar a la función y mostrar el máximo hallado, o un mensaje
 informativo si éste no existe.
 """
 
+from functools import reduce
+
 
 def numeros_input() -> list[int]:
-    """Pide y guarda 3 numeros positivos.
-    Post -> Devuelve una lista con los tres numeros.
+    """Pide y guarda 3 números positivos en una lista.
+
+    Post: Devuelve una lista que contiene exactamente tres números enteros
+          positivos.
+
     """
     numeros = []
-    i = 1
     while len(numeros) < 3:
         try:
-            numero = int(input(f"ingrese el numero {i}: "))
+            numero = int(input(f"Ingrese el número {len(numeros)+1}: "))
             if numero > 0:
                 numeros.append(numero)
-                i += 1
             else:
-                print("El numero tiene que ser positivo.")
+                print("El número tiene que ser positivo.")
         except ValueError:
-            print("Valor invalido, reintentar.")
+            print("Valor inválido, reintentar.")
     return numeros
 
 
 def mayor(numeros: list[int]) -> int:
-    """Busca el mayor estricto de los 3 y devuelve el numero mayor o -1
-    si no es el mayor estricto.
-    Pre -> Recibe una lista de enteros con los 3 numeros mayores a 0
-    Post -> Devuelve el mayor estricto si existe, sino devuelve -1
+    """Busca el mayor estricto de los 3 y devuelve el número mayor o -1 si no es
+    el mayor estricto.
+
+    Pre: Recibe una lista de enteros con los 3 números mayores a 0.
+
+    Post: Devuelve el mayor estricto si existe, si no devuelve -1.
+
     """
-    mayor = 0
-    rep = 0
-    for i in numeros:
-        if i > mayor:
-            mayor = i
-            rep = 0
-        elif i == mayor:
-            rep += 1
-    return mayor if rep == 0 else -1
+    n_mayor = reduce(lambda x, y: x if x > y else y, numeros)
+    rep = numeros.count(n_mayor) - 1
+    return n_mayor if rep == 0 else -1
 
 
 def imprimir_resultados(resultado: int) -> None:
     """Imprime los resultados obtenidos de los procesos anteriores.
-    Pre -> Recibe el resultado de mayor().
-    Post -> Si el resultado es distinto a -1 imprime el mayor estricto.
+
+    Pre: Recibe el resultado de mayor(), un número entero.
+
+    Post: Si resultado es distinto a -1 imprime el mayor estricto.
+
     """
     if resultado != -1:
-        print(f"El numero mayor ingresado es: {resultado}.")
+        print(f"El número mayor ingresado es: {resultado}.")
     else:
-        print("No hubo ningun numero mayor estricto.")
+        print("No hubo ningún número mayor estricto.")
 
 
 def main() -> None:
