25375eba2ecf4b8caa865f17a1bb0b55335a5d10
mauwqq <mauw0@protonmail.com>
2024-09-22 21:11:56 -0300

    Terminado y documentado

---
+++ TPs/TP4/E8.py
@@ -0,0 +1,55 @@
+"""
+Desarrollar una función que devuelva una subcadena con los últimos N caracteres
+de una cadena dada. La cadena y el valor de N se pasan como parámetros.
+"""
+
+
+def pedir_string() -> str:
+    """Solicita al usuario que ingrese un texto y lo devuelve.
+
+    Post: Retorna la cadena de texto ingresada por el usuario.
+    
+    """
+    return input("Ingrese el texto: ")
+
+
+def pedir_num() -> int:
+    """Solicita al usuario un número entero positivo y lo devuelve.
+
+    Post: Retorna el valor ingresado si es un número entero positivo.
+    
+    """
+    while True:
+        try:
+            n = int(input("Ingrese cuantos caracteres quiere ver de la cadena: "))
+            if n > 0:
+                break
+            print("El número tiene que ser positivo.")
+        except ValueError:
+            print("Debe ingresar un número.")
+    return n
+
+
+def ultimos_n_caracteres(cadena: str, n: int) -> str:
+    """Devuelve los últimos N caracteres de la cadena dada.
+
+    Pre: cadena es la cadena de texto de la cual se extraerán los caracteres.
+         n es el número de caracteres a extraer desde el final.
+
+    Post: Retorna una subcadena que contiene los últimos N caracteres de la
+          cadena.
+    
+    """
+    if n > len(cadena):
+        n = len(cadena)
+    return ''.join([cadena[i] for i in range(len(cadena) - n, len(cadena))])
+
+
+def main() -> None:
+    cadena = pedir_string()
+    n = pedir_num()
+    print(ultimos_n_caracteres(cadena, n))
+
+
+if __name__ == '__main__':
+    main()
